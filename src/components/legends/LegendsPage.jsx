490b556c18161d64935cf91fdff5b674275e66bf
rameem2003 <rameem2019@gmail.com>
2023-07-29 10:20:10 +0600

    slider added

--- src/components/legends/LegendsPage.jsx
+++ src/components/legends/LegendsPage.jsx
@@ -1,11 +1,53 @@
 import React, { useState } from "react";
 import { FaFacebook, FaGithub } from "react-icons/fa6";
-
-import "./legendspage.css";
+import Slider from "react-slick";
 
 import { legendsData } from "../../data/legendsData";
+import "./legendspage.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function LegendsPage() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 3000,
+    cssEase: "linear",
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   const [legends, setLegedns] = useState(legendsData);
 
   return (
@@ -16,32 +58,34 @@ function LegendsPage() {
         </div>
 
         <div className="members-list" id="members-list">
-          {legends.map((legend) => {
-            return (
-              <div className="member">
-                <div class="img">
-                  <img src={legend.img} alt="" />
-                </div>
+          <Slider {...settings}>
+            {legends.map((legend) => {
+              return (
+                <div className="member">
+                  <div class="img">
+                    <img src={legend.img} alt="" />
+                  </div>
 
-                <div class="name">
-                  <h3>{legend.name}</h3>
-                </div>
+                  <div class="name">
+                    <h3>{legend.name}</h3>
+                  </div>
 
-                <div class="post">
-                  <h4>{legend.position}</h4>
-                </div>
+                  <div class="post">
+                    <h4>{legend.position}</h4>
+                  </div>
 
-                <div class="link">
-                  <a href={legend.facbook}>
-                    <FaFacebook/>
-                  </a>
-                  <a href={legend.github}>
-                    <FaGithub/>
-                  </a>
+                  <div class="link">
+                    <a href={legend.facbook}>
+                      <FaFacebook />
+                    </a>
+                    <a href={legend.github}>
+                      <FaGithub />
+                    </a>
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </Slider>
         </div>
       </div>
     </div>
